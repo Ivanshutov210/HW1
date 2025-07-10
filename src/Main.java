b43b8319d515edd84ddf18402d214c3d0834f15a
Иван <ivan.shutov95@mail.ru>
2025-07-10 16:06:30 +0300

    Hw1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Задача 1");
+        int a = 1;
+        byte b = 16;
+        short c = 2200;
+        long d =  123_212_421_123L;
+        float e = 3.75f;
+        double f  = 4.346;
+        System.out.printf("Значение переменной" + a );
+        System.out.printf("Значение переменной" + b );
+        System.out.printf("Значение переменной" + c );
+        System.out.printf("Значение переменной" + d );
+        System.out.printf("Значение переменной" + e );
+        System.out.printf("Значение переменной" + f );
+
+    }
+    }
