9f915ffba65beea3491d65b7fadc11852a219b72
Иван <ivan.shutov95@mail.ru>
2025-07-11 16:23:54 +0300

    Ready HW

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Задача 1");
+        System.out.print("Задача 1.");
         int a = 1;
         byte b = 16;
         short c = 2200;
@@ -16,7 +16,7 @@ public class Main {
         System.out.printf("Значение переменной" + e );
         System.out.printf("Значение переменной" + f );
 
-        System.out.printf("Задача 2");
+        System.out.print("Задача 2.");
         float g = 27.12f;
         long  h = 987_678_965_549L;
         double i = 2.786;
@@ -32,16 +32,16 @@ public class Main {
         System.out.printf("Значение переменной" + l );
         System.out.printf("Значение переменной" + m );
 
-        System.out.printf("Задача 3");
+        System.out.print("Задача 3.");
         byte Lyudmilasclass =  23;
         byte Annasclass = 27;
         byte Ekaterinasclass = 30;
         int Threeclasses = Lyudmilasclass + Annasclass + Ekaterinasclass;
         short Paperforthreeclasses = 480;
         int Sheetforastdent = Paperforthreeclasses /  Threeclasses;
-        System.out.printf("На каждого ученика рассчитано" + Sheetforastdent + "листов бумаги");
+        System.out.printf("На каждого ученика рассчитано" + Sheetforastdent + "листов бумаги.");
 
-        System.out.printf("Задача 4");
+        System.out.print("Задача 4.");
         byte twominutes = 2;
         byte bottlesintwominutes = 16;
         int  bottleperminute = bottlesintwominutes / twominutes;
@@ -56,12 +56,12 @@ public class Main {
         short per1daybottles = (short) (mintsinoneday * bottleperminute);
         int  per3daybottles = (mintsinthosedays *  bottleperminute);
         int per30daybottles = (minutespermonth *  bottleperminute);
-        System.out.printf("За 20 минут машина произвела" + in20minutesbottles + "штук бутылок");
-        System.out.printf("За 1 день машина произвела" + per1daybottles + "штук бутылок");
-        System.out.printf("За 3 дня машина произвела" + per3daybottles + "штук бутылок");
-        System.out.printf("За 30  дней машина произвела" + per30daybottles + "штук бутылок");
+        System.out.printf("За 20 минут машина произвела" + in20minutesbottles + "штук бутылок.");
+        System.out.printf("За 1 день машина произвела" + per1daybottles + "штук бутылок.");
+        System.out.printf("За 3 дня машина произвела" + per3daybottles + "штук бутылок.");
+        System.out.printf("За 30  дней машина произвела" + per30daybottles + "штук бутылок.");
 
-        System.out.printf("Задача 5");
+        System.out.print("Задача 5.");
         byte Allthepaint = 120;
         byte Whitepaintforoneclass = 2;
         byte Brownpaintforoneclass = 4;
@@ -70,7 +70,7 @@ public class Main {
         int brown = Totaclasses * Brownpaintforoneclass;
         System.out.printf("В школе, где" + Totaclasses + "классов, нужно" +  white + "банок белой краски и" + brown + "банок коричневой краски");
 
-        System.out.printf("Задача 6");
+        System.out.print("Задача 6.");
         short Bananas = 5 * 80;
         short milk = 105 * 2;
         short iseCream = 100 * 2;
@@ -79,7 +79,7 @@ public class Main {
         float Kg = grams / 1000f;
         System.out.printf("Вес завтрака состовляет" + grams + "грам и" + Kg + "килограм");
 
-        System.out.printf("Задача 7");
+        System.out.print("Задача 7.");
         short loseagram = 7 * 1000;
         short firstresetgram = 250;
         short secondresetgram = 500;
@@ -88,7 +88,26 @@ public class Main {
         System.out.printf("количесво дней для похудения при потере 250 грамм в день"+ lose250aday);
         System.out.printf("количесво дней для похудения при потере 500 грамм в день"+ lose500aday);
         int average = (lose500aday + lose250aday) / 2;
-        System.out.printf("Среднее колличество дней для похудения" + average);
+        System.out.printf("Среднее колличество дней для похудения." + average);
 
+        System.out.print("Задача 8.");
+        int Masha = 67760;
+        int Denis= 83690;
+        int Kristina = 76230;
+        float salaryincreaseMasha = (float)(67760 * 0.1);
+        float salaryincreaseDenis = (float)(83690 * 0.1);
+        float salaryincreaseKritina = (float)(76230 * 0.1);
+        int newsalaryMasha = (int)(Masha + salaryincreaseMasha);
+        int newsalaryDenis = (int)(Denis + salaryincreaseDenis);
+        int newsalaryKristina = (int)(Kristina + salaryincreaseKritina);
+        float mashaYear = Masha * 12;
+        float denisYear = Denis * 12;
+        float kristinaYear = Kristina * 12;
+        int masha = (int) (mashaYear - newsalaryMasha);
+        int denis = (int) (denisYear - newsalaryDenis);
+        int kristina = (int) (kristinaYear - newsalaryKristina);
+        System.out.print("Маша теперь получает" + newsalaryMasha + "рублей. Годовой доход вырос на" + masha + "рублей.");
+        System.out.print("Динис теперь получает" + newsalaryDenis + "рублей. Годовой доход вырос на" + denis + "рублей.");
+        System.out.print("Кристина теперь получает" + newsalaryKristina + "рублей. Годовой доход вырос на" + kristina + "рублей.");
     }
 }
